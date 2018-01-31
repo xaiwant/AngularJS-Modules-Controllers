59774f3b817b5a808c08b4a059bd0318a7e51daf
Jaywant Topno <jaywanttopno@yahoo.com>
2018-01-31 21:20:49 +0530

    Initial commit to push codebase.

---
+++ ModulesControllers/modulescontrollers.js
@@ -0,0 +1,5 @@
+
+	var myApp = angular.module('myApp', []);
+        myApp.controller('myController', function($scope) {
+               $scope.text = 'Hello everyone\, Welcome to AngularJS learning Tutorial.';
+	    });
